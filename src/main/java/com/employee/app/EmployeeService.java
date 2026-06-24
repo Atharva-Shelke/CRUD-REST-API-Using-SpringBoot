9c8c5a250d2ff6b0efb966f4c322aaea03d67eb7
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-06-24 12:09:02 +0530

    Added pagination using Pageable

--- src/main/java/com/employee/app/EmployeeService.java
+++ src/main/java/com/employee/app/EmployeeService.java
@@ -1,6 +1,9 @@
 package com.employee.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -31,5 +34,12 @@ public class EmployeeService {
 
 	public void delete(int id) {
 		repository.deleteById(id);
+	}
+
+	public Page<Employee> getEmployees(int page, int size) {
+
+		Pageable pageable = PageRequest.of(page, size, Sort.by("id").ascending());
+
+		return repository.findAll(pageable);
 	}
 }
