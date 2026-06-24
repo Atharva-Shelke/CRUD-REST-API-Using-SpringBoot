0dd8bb9e371bd805c9a90204ba443cb9f02d52e3
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-06-24 12:51:47 +0530

    Added endpoint to call paginated result

--- src/main/java/com/employee/app/MyController.java
+++ src/main/java/com/employee/app/MyController.java
@@ -3,6 +3,7 @@ package com.employee.app;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -65,5 +67,11 @@ public class MyController {
 		} catch (RuntimeException e) {
 			return ResponseEntity.status(404).body(e.getMessage());
 		}
+	}
+
+	@GetMapping("/page")
+	public Page<Employee> getEmployees(@RequestParam int page, @RequestParam int size) {
+
+		return service.getEmployees(page, size);
 	}
 }
